bef55db329a41d07bae762fe440cd3751d9f7772
Cody Danielson <cdanie10@uwyo.edu>
2025-05-11 07:40:16 -0600

    Add separate token logging function and store/log token totals

--- monadic/interactive_session.py
+++ monadic/interactive_session.py
@@ -18,10 +18,24 @@ class Interact:
         self.timeline      = event_timeline.Timeline()
 
 
+
     def respond(self, content) -> str:
         self.timeline.add_history('user', content)
-        logger.info('\033[35m'+'},\n'.join(str(self.timeline.get_history()).split('},'))+'\033[0m')
+        logger.info('\n'+'\033[35m'+'},\n'.join(str(self.timeline.get_history()).split('},'))+'\033[0m')
+
         response = interactions.responses(self.timeline.get_history())
-        logger.info(f'\033[35minput tokens: {response.usage.input_tokens}; output tokens: {response.usage.output_tokens}\033[0m')
+        if response.usage: self.update_tokens(response.usage.input_tokens, response.usage.output_tokens)
+
         self.timeline.add_history('assistant', response.output_text)
         return response.output_text
+
+
+
+    def update_tokens(self, input_tokens, output_tokens) -> None:
+        self.input_tokens += input_tokens
+        self.output_tokens += output_tokens
+        token_string =  f'\n\033[35minput tokens:        {input_tokens}\033[0m'
+        token_string += f'\n\033[35moutput tokens:       {output_tokens}\033[0m'
+        token_string += f'\n\033[35mtotal input tokens:  {self.input_tokens}\033[0m'
+        token_string += f'\n\033[35mtotal output tokens: {self.output_tokens}\033[0m'
+        logger.info(token_string)
