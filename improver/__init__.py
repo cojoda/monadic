851c95e69582030e3befab5fd93bb93410e134f5
Cody Danielson <cody.danielson@icloud.com>
2025-08-09 23:09:08 -0600

    chore: Remove v1 code to prepare main for v2 development

--- improver/__init__.py
+++
@@ -1,5 +0,0 @@
-# improver/__init__.py
-
-from .orchestrator import Improver
-
-__all__ = ["Improver"]
--- tests/test_placeholder.py
+++
@@ -1,3 +0,0 @@
-def test_placeholder() -> None:
-    """A simple placeholder test to verify pytest is configured correctly."""
-    assert True
